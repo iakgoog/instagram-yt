196375e0497abf1f478ab52654bd1549d8f939f9
Sutthinart Khunvadhana <iakgoog@gmail.com>
2021-09-01 07:47:02 +0700

    feat: scaffold dashboard

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import * as ROUTES from './constants/routes';
 
 const Login = lazy(() => import('./pages/login'));
 const Signup = lazy(() => import('./pages/sign-up'));
+const Dashboard = lazy(() => import('./pages/dashboard'));
+const NotFound = lazy(() => import('./pages/not-found'));
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login} />
           <Route path={ROUTES.SIGN_UP} component={Signup} />
+          <Route path={ROUTES.DASHBOARD} component={Dashboard} />
           <Route component={NotFound} />
         </Switch>
       </Suspense>
